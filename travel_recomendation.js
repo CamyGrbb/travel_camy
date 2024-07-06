d03248e52b1d587ff62140e84310b3c33adf1cff
Cesar Gonzalez <cegor79@gmail.com>
2024-07-06 18:56:16 -0400

    Updated file travel_recomendation.js

--- travel_recomendation.js
+++ travel_recomendation.js
@@ -1,38 +1,75 @@
 const btnSearch = document.getElementById("btnSearch");
 const btnClear = document.getElementById("btnClear");
+const resultDiv = document.getElementById("result");
 
 function searchRecomendation() {
-  const input = document.getElementById("recomendationInput").value.toLowerCase();
-  const resultDiv = document.getElementById("result");
-  resultDiv.innerHTML = "";
+    const input = document.getElementById("recomendationInput").value.toLowerCase();
+    resultDiv.innerHTML = "";
 
-  fetch("./travel_recomendation.json")
-    .then((response) => response.json())
-    .then((data) => {
-      const countries = data.countries.find(
-        (item) => item.toLowerCase() === input
-      );
+    fetch("./travel_recomendation.json")
+        .then((response) => response.json())
+        .then((data) => {
+            const { countries, temples, beaches } = data
+            const { cities } = countries.find((item) => item.name.toLowerCase() === input)
 
-      if (countries) {
-        const cities = countries.cities.forEach(city => {
-            
+            if (cities !== undefined) {
+                displayCities(cities)
+            } else if (input === "temples" | "temple") {
+                displayTemples(temples)
+            } else if (input === "beaches" | "beach") {
+                displayBeaches(beaches)
+            } else {
+                resultDiv.innerHTML = "Result not found.";
+            }
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+            resultDiv.innerHTML = "An error occurred while fetching data.";
         });
-        const prevention = countries.prevention.join(", ");
-        const treatment = countries.treatment;
-
-        resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-        resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
-
-        resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-        resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-        resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
-      } else {
-        resultDiv.innerHTML = "Condition not found.";
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      resultDiv.innerHTML = "An error occurred while fetching data.";
-    });
 }
-btnSearch.addEventListener("click", searchCondition);
+btnSearch.addEventListener("click", searchRecomendation);
+
+function displayCities(citiesData) {
+    const cities = citiesData.map((city) => `
+        <div class="card" style="width: 18rem;">
+            <img src="${city.imageUrl}" class="card-img-top" alt="${city.name}">
+            <div class="card-body">
+                <h5 class="card-title">${city.name}</h5>
+                <p class="card-text">${city.name}</p>
+                <a href="#" class="btn btn-primary">Info</a>
+            </div>
+        </div>
+        `
+    )
+    resultDiv.innerHTML = cities
+}
+
+function displayTemples(templesData) {
+    const temples = templesData.map((temple) => `
+        <div class="card" style="width: 18rem;">
+            <img src="${temple.imageUrl}" class="card-img-top" alt="${temple.name}">
+            <div class="card-body">
+                <h5 class="card-title">${temple.name}</h5>
+                <p class="card-text">${temple.name}</p>
+                <a href="#" class="btn btn-primary">Info</a>
+            </div>
+        </div>
+        `
+    )
+    resultDiv.innerHTML = temples
+}
+
+function displayBeaches(beachesData) {
+    const beaches = beachesData.map((beach) => `
+        <div class="card" style="width: 18rem;">
+            <img src="${beach.imageUrl}" class="card-img-top" alt="${beach.name}">
+            <div class="card-body">
+                <h5 class="card-title">${beach.name}</h5>
+                <p class="card-text">${beach.name}</p>
+                <a href="#" class="btn btn-primary">Info</a>
+            </div>
+        </div>
+        `
+    )
+    resultDiv.innerHTML = beaches
+}
